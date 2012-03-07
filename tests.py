78dca161f526588e6fd1651753b9999f5e492156
Fabio Souto <fsoutomoure@gmail.com>
2012-03-07 03:10:31 +0100

    first commit

---
+++ tests.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*- 
+
+import unittest
+import pyWebHDFS
+
+class pyWebHDFSTests(unittest.TestCase):
+
+	def setUp(self):
+		self.webHDFS = pyWebHDFS.pyWebHDFS('localhost', 50070, 'fabio')
+		self.test_dir = '/pywebhdfs_test'
+
+	def testCreateDir(self):
+		self.webHDFS.mkdir(self.test_dir , '777')
+		dir_content = self.webHDFS.listdir('/')
+		dir_filenames = (d['pathSuffix'] for d in dir_content)
+		self.assertTrue('pywebhdfs_test' in dir_filenames)
+	
+	def testChmod(self):
+		self.webHDFS.mkdir(self.test_dir , '777')
+		self.webHDFS.chmod(self.test_dir, '444')
+	 	dir_content = self.webHDFS.listdir('/')
+	 	created_dir = [d for d in dir_content if d['pathSuffix'] == 'pywebhdfs_test']
+	 	self.assertEqual(created_dir[0]['permission'], '444')
+
+	def tearDown(self):
+	 	self.webHDFS.remove(self.test_dir, True)
+
+if __name__ == '__main__':
+	unittest.main()
