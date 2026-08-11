a475425b644a1c2fa6b8e010480c42fd7b9f6cfb
Fabio Souto <fsoutomoure@gmail.com>
2026-08-12 01:08:13 +0200

    Un-vendor the alabaster Sphinx theme
    html_theme_path was commented out, so the vendored copy was never used;
    Sphinx resolved alabaster from the installed package. Depend on it
    explicitly instead. Also fixes the two build warnings.

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 #
 # webhdfspy documentation build configuration file, created by
 # sphinx-quickstart on Thu Apr 23 18:39:29 2015.
@@ -12,15 +11,13 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys
 import os
-import shlex
+import sys
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
-import webhdfspy
 
 
 # -- General configuration ------------------------------------------------
@@ -68,7 +65,7 @@ release = '1.0.0'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
@@ -141,7 +138,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -226,8 +223,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'webhdfspy.tex', u'webhdfspy Documentation',
-   u'Fabio Souto', 'manual'),
+  (master_doc, 'webhdfspy.tex', 'webhdfspy Documentation',
+   'Fabio Souto', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -256,7 +253,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'webhdfspy', u'webhdfspy Documentation',
+    (master_doc, 'webhdfspy', 'webhdfspy Documentation',
      [author], 1)
 ]
 
@@ -270,7 +267,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'webhdfspy', u'webhdfspy Documentation',
+  (master_doc, 'webhdfspy', 'webhdfspy Documentation',
    author, 'webhdfspy', 'One line description of project.',
    'Miscellaneous'),
 ]
